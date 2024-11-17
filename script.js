4a97ccbc21468eb6ec21957139ed99d0b8349c21
ReStart-Academy <restart18112024@gmail.com>
2024-11-16 23:20:58 -0800

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+var player_score = 0;
+var computer_score = 0;
 function playGame(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-    
-    document.getElementById('player-choice').textContent = `Your choice: ${playerChoice}`;
-    document.getElementById('computer-choice').textContent = `Computer's choice: ${computerChoice}`;
-    
+
+
+
+    document.getElementById('player-choice').innerHTML = `<i class="fa-solid fa-hand-${playerChoice}"></i>`
+    document.getElementById('computer-choice').innerHTML = `<i class="fa-solid fa-hand-${computerChoice}" style="background-color: red;"></i>`;
+
+
     let result = '';
 
     if (playerChoice === computerChoice) {
@@ -14,10 +19,15 @@ function playGame(playerChoice) {
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
-        result = 'You win!';
+        result = 'You win!!!';
+        player_score += 1;
+
     } else {
-        result = 'You lose!';
+        result = 'You lose!!';
+        computer_score += 1;
     }
 
     document.getElementById('result').textContent = `Result: ${result}`;
+    document.getElementById('ply_score').textContent = `Your Score: ${player_score}`;
+    document.getElementById('com_score').textContent = `Computer's Score: ${computer_score}`;
 }
